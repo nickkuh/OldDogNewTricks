3dfbc8f5eb111d9eee0d37f810a9a9bab98ae218
Nick Kuh <nick.kuh@gmail.com>
2015-04-20 18:30:25 +0100

    Added closure example

---
+++ Closures - Download an Image/Closures - Download an Image/AppDelegate.swift
@@ -0,0 +1,89 @@
+//
+//  AppDelegate.swift
+//  Closures - Download an Image
+//
+//  Created by Nick Kuh on 20/04/2015.
+//  Copyright (c) 2015 Mumbo Apps Ltd. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        
+        func downloadAnImage(url:NSURL,completion:(image:UIImage?,error:NSError?)->Void) {
+            //download the image
+            println("starting download")
+            var request: NSURLRequest = NSURLRequest(URL: url)
+            let config = NSURLSessionConfiguration.defaultSessionConfiguration()
+            let session = NSURLSession(configuration: config)
+            
+            
+            let downloadTask : NSURLSessionDataTask = session.dataTaskWithRequest(request, completionHandler: {(data, response, error) in
+                
+                if data != nil {
+                    
+                    var img = UIImage(data: data);
+                    
+                    completion(image: img, error: nil)
+                }
+                else {
+                    completion(image: nil, error: error)
+                }
+                
+            })
+            downloadTask.resume()
+            
+        }
+        
+        let url = NSURL(string: "http://placehold.it/350x350")
+        downloadAnImage(url!) { (image, error) -> Void in
+            if image != nil {
+                println("downloaded the image!")
+                
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.window?.rootViewController?.view.addSubview(UIImageView(image: image))
+                }
+                
+                
+            }
+            else {
+                println("error downloading: \(error)")
+            }
+        }
+
+        
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    }
+
+    func applicationDidEnterBackground(application: UIApplication) {
+        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
+        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(application: UIApplication) {
+        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
+    }
+
+    func applicationDidBecomeActive(application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(application: UIApplication) {
+        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    }
+
+
+}
+
